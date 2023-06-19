9d1f76351315eb5eef7f2a98985d5897de131d46
prince1382002kumar <prince1382002kumar@gmail.com>
2023-06-19 18:41:19 +0530

    adding contact form

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ var typed = new Typed('#element', {
     strings: ['Web Designer. ^800', 'Programmer. ^800', 'Engineer. ^800', 'Developer. ^800'],
     typeSpeed: 40, loop: true,
 });
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gamil.com",
+        Username : "mail.princekumar.0@gmail.com",
+        Password : "password",
+        To : 'mail.princekumar.0@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Enquiry",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}

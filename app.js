94fd9b2028d9493782c68e13306ec01c8fcacaf3
Prince Kumar <prince1382002kumar@gmail.com>
2023-06-20 15:31:35 +0530

    contact form formspree enabled

--- app.js
+++ app.js
@@ -3,16 +3,58 @@ var typed = new Typed('#element', {
     typeSpeed: 40, loop: true,
 });
 
-function sendEmail(){
-    Email.send({
-        Host : "smtp.gamil.com",
-        Username : "mail.princekumar.0@gmail.com",
-        Password : "password",
-        To : 'mail.princekumar.0@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "New Contact Form Enquiry",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}
+
+    // const scriptURL = 'https://script.google.com/macros/s/AKfycbzTNboTdEgDZUCJ6hl5TiH845ocVtKIa_Wv6f2sSE5KIN-y-UIfkzw0h-DOrzTgGDU9/exec'
+    // const form = document.forms['submit-to-google-sheet']
+
+    // form.addEventListener('submit', e => {
+    //     e.preventDefault()
+    //     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    //     .then(response => console.log('Success!', response))
+    //     .catch(error => console.error('Error!', error.message))
+    // })
+
+// function sendEmail(){
+//     Email.send({
+//         Host : "smtp.gamil.com",
+//         Username : "mail.princekumar.0@gmail.com",
+//         Password : "password",
+//         To : 'mail.princekumar.0@gmail.com',
+//         From : document.getElementById("email").value,
+//         Subject : "New Contact Form Enquiry",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
+
+// function submitform(event){
+//     event.preventDefault();
+
+//     var name=document.getElementById("name").value;
+//     var message=document.getElementById("inputmessage").value;
+//     var phone=document.getElementById("phone").value;
+
+//     var combinedValue = "Name :" + name + "Phone no. :" + phone + "Message :" + message;
+//     document.getElementById("combinedValue").value = combinedValue;
+    
+    
+//     // fetch("https://formspree.io/f/xoqzgybd", {
+//     //         method: "POST",
+//     //         body: combinedValue
+//     //     })
+//     //         .then(response => {
+//     //             // Handle the response from the CDN action if needed
+//     //             console.log("CDN action completed");
+//     //         })
+//     //         .catch(error => {
+//     //             console.error("Error:", error);
+//     //         });
+//     window.location.method = "post";
+//     window.location.href = "https://formspree.io/f/xoqzgybd";
+//     document.getElementById("contactform").reset();
+// }
+
+
+// https://script.google.com/macros/s/AKfycbzTNboTdEgDZUCJ6hl5TiH845ocVtKIa_Wv6f2sSE5KIN-y-UIfkzw0h-DOrzTgGDU9/exec
+    

f581c0296918fb1de006f23a858a417fc217861a
prince1382002kumar <prince1382002kumar@gmail.com>
2023-07-17 14:55:52 +0530

    Fixed the Contact Form
    By using a second form I combined all the inputs to two inputs according to formspree format

--- app.js
+++ app.js
@@ -3,16 +3,3 @@ var typed = new Typed('#element', {
     typeSpeed: 40, loop: true,
 });
 
-function sendEmail(){
-    Email.send({
-        Host : "smtp.gamil.com",
-        Username : "mail.princekumar.0@gmail.com",
-        Password : "password",
-        To : 'mail.princekumar.0@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "New Contact Form Enquiry",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}
